8c709ea3fca47b2b448e6e5f162dcad8bf2e497a
nikhil-n-more <44098492+nikhil-n-more@users.noreply.github.com>
2024-07-16 10:42:50 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -2,3 +2,54 @@ shutdown = ['close', 'shutdown']
 
 play_music = ['music', 'songs', 'song', 'beats']
 
+predefined_commands = [
+    # what quests
+    "what location",
+    "what name",
+    "what favourite color",
+    "what favourite food",
+    "what favourite time pass",
+    "what favourite destination",
+    "what favourite work"
+
+    # find commands
+    "find location",
+    "find product",
+
+    # open commands
+    "open youtube",
+    "open classroom",
+    "open mail",
+    "open whatsapp",
+    "open system monitor",
+    "open code"
+
+    # play commands
+    "play youtube"
+
+    # search commands
+    "search youtube",
+    "search web for",
+    "fetch details about",
+    "get details about",
+
+    # fetch information commands
+    "fetch info",
+    "fetch information",
+
+    # create projects/ folder commands
+    "create project",
+    "create file",
+    "create folder",
+
+    # install commands
+    "install python package",
+    "install ",
+    
+    # download commands
+    "download from url",
+
+    # shut down commands
+    "close"
+
+]
